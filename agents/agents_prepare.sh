49e7af6c32b3adb051f0dbcdd9ccbacf5bd34d3f
kevangel79 <58726215+kevangel79@users.noreply.github.com>
2020-01-07 14:24:45 +0200

    Delete agents_prepare.sh

--- agents/agents_prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Run Script on new docker agent for quick setup
-yum update -y
-yum install -y java-1.8.0-openjdk git
-yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install -y docker-ce docker-ce-cli containerd.io
