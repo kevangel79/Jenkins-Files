4903a775227a411c74c72375c2a8c2f8bcc26327
kevangel79 <58726215+kevangel79@users.noreply.github.com>
2020-07-10 16:29:10 +0300

    Update backup-vol.sh

--- backup/backup-vol.sh
+++ backup/backup-vol.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 DATE=`date +%d-%m-%y`
-docker run --rm --volumes-from jekins-data \
-        -v $(pwd):/backup ubuntu \
-        tar cvf /backup/backup_${DATE}.tar /var/jenkins_home
-
-#scp ./backup_${DATE}.tar remotehost:/backup-jenkins/
+docker run --rm --volumes-from deployment_jenkins_1 \
+        -v $(pwd)/thinBackups:/backup ubuntu \
+        tar cvf /backup/backup_${DATE}.tar /var/jenkins_home/backups/
